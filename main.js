27ffde7ccc26bc41b0572581ca36e4006ff248e0
Andrew Church <andrewchurch@Andrews-MacBook-Pro.local>
2017-08-15 13:19:46 -0600

    Add existing file

--- main.js
+++ main.js
@@ -36,22 +36,25 @@ tomato.stringify()
 bun.stringify()
 
 
-var Plate = function(name, description, price, ingredients){
+var Plate = function(name, description, price, ingredients, calories, vegan, glutenFree){
 	this.name = name
 	this.description = description
 	this.price = price
 	this.ingredients = ingredients
+	this.calories = calories
+	this.vegan = vegan
+	this.glutenFree = glutenFree
 }
-var baconBurger = new Plate('bacon burger', '100% angus beef burger with bacon', 12, FoodItem[0,1,2,3])
-var greenSalad = new Plate('green salad', 'green salad with tomatoes and bacon bits', 9, FoodItem[0,1,2])
+var baconBurger = new Plate('bacon burger', '100% angus beef burger with bacon', 12, 'bacon, lettuce, tomato, bun', 191, false, false)
+var greenSalad = new Plate('green salad', 'green salad with tomatoes and bacon bits', 9, 'lettuce, tomato, bacon', 81, false, true)
 
 
 var Menu = function(name, plates){
 	this.name = name
 	this.plates = plates
 }
-var lunchMenu = new Menu('lunch menu', Plate[0,1])
-var dinnerMenu = new Menu('dinner menu', Plate[1])
+var lunchMenu = new Menu('lunch menu', 'bacon burger, green salad')
+// var dinnerMenu = new Menu('dinner menu', Plate[1])
 
 var Restaurant = function(name, description, menu){
 	this.name = name
@@ -61,14 +64,32 @@ var Restaurant = function(name, description, menu){
 var BobsBurgers = new Restaurant('Bobs Burgers', 'a burger joint', lunchMenu)
 
 Plate.prototype.stringify = function(){
-
+	if(this.vegan === true && this.glutenFree === true){
+		console.log(this.name + ': ' + ' Ingredients: ' + this.ingredients + '. ' + 'Price: $' + this.price + '.' + ' Calories: ' + this.calories + '.' + ' *This food is Vegan. ' + '*This food is gluten free.')
 }
+	else if(this.vegan === false && this.glutenFree === true){
+		console.log(this.name + ': ' + ' Ingredients: ' + this.ingredients + '. ' + 'Price: $' + this.price + '.' + ' Calories: ' + this.calories + '.' + ' *This food is NOT Vegan. ' + '*This food is gluten free.')
+	}
+	else if(this.vegan === true && this.glutenFree === false){
+		console.log(this.name + ': ' + ' Ingredients: ' + this.ingredients + '. ' + 'Price: $' + this.price + '.' + ' Calories: ' + this.calories + '.' + ' *This food is Vegan. ' + '*This food is NOT gluten free.')
+	}
+	else {
+		console.log(this.name + ': ' + ' Ingredients: ' + this.ingredients + '. ' + 'Price: $' + this.price + '.' + ' Calories: ' + this.calories + '.' + ' *This food is NOT Vegan. ' + '*This food is NOT gluten free.')
+	}
+}
+// greenSalad.stringify()
+// baconBurger.stringify()
 
 Menu.prototype.stringify = function(){
-
+	console.log(this.name + ': ' + this.plates)
 }
+// lunchMenu.stringify()
 
 Restaurant.prototype.stringify = function(){
+	console.log(this.name + ': ')
 
 }
-
+BobsBurgers.stringify()
+lunchMenu.stringify()
+baconBurger.stringify()
+greenSalad.stringify()
